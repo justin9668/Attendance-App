bc783cc486bececb82f2fe15484df84a28f9c0c1
Justin Wang <justin1@bu.edu>
2024-04-15 22:14:38 -0400

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [code, setCode] = useState('')
+  const [qrCodeUrl, setQrCodeUrl] = useState('')
+
+  const fetchCode = () => {
+    fetch('http://127.0.0.1:8080/api/code')
+      .then(response => response.text())
+      .then(data => {
+        setCode(data)
+        setQrCodeUrl('')
+      })
+      .catch(error => console.error('Error fetching code:', error))
+  }
+
+  const fetchQRCode = () => {
+    fetch('http://127.0.0.1:8080/api/qrcode')
+      .then(response => {
+        if (response.ok) {
+          return response.blob()
+        }
+        throw new Error('Failed to generate QR code.')
+      })
+      .then(blob => {
+        const url = URL.createObjectURL(blob)
+        setQrCodeUrl(url);
+      })
+      .catch(error => console.error('Error:', error))
+  }
+
+  return (
+    <div className="App">
+      <button onClick={fetchCode}>Generate Code</button>
+      {code && <p>Code: {code}</p>}
+      {code && !qrCodeUrl && <button onClick={fetchQRCode}>Generate QR Code</button>}
+      {qrCodeUrl && <img src={qrCodeUrl} alt="QR Code" />}
+    </div>
+  )
+}
+
+export default App
---
+++ server/main.py
@@ -0,0 +1,40 @@
+from flask import Flask, send_file
+from flask_cors import CORS
+from io import BytesIO
+import random
+import requests
+
+app = Flask(__name__)
+cors = CORS(app, origins='*')
+
+code = None
+
+@app.route("/")
+def home():
+    return "Here"
+
+@app.route("/api/code", methods=['GET'])
+def code():
+    global code
+    code = random.randint(1000, 9999)
+    return str(code)
+
+@app.route("/api/qrcode", methods=['GET'])
+def qrcode():
+    global code
+    if code is None:
+        return "No code generated yet.", 400
+
+    size = "200x200"
+    api_url = f"https://api.qrserver.com/v1/create-qr-code/?data={code}&size={size}"
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        img = BytesIO(response.content)
+        img.seek(0)
+        code = None
+        return send_file(img, mimetype='image/png', as_attachment=False, download_name=f"qr_{code}.png")
+    else:
+        return "Failed to generate QR Code."
+    
+if __name__ == "__main__": 
+    app.run(debug=True, port=8080)
